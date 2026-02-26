5db1ceaa8779e51d6f1865934c3f0cdf22c0a932
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2026-02-26 14:59:32 +0500

    Update Message.tsx

--- src/components/React/ChatWidget/Elements/Message.tsx
+++ src/components/React/ChatWidget/Elements/Message.tsx
@@ -22,7 +22,7 @@ export function Message({ message }: TMessage){
             }`}
         >
             <div
-                className={`max-w-[75%] px-4 py-2.5 text-sm ${
+                className={`max-w-[80%] px-4 py-2.5 text-xs sm:text-sm ${
                     message.type === "user"
                     ? "text-white rounded-2xl rounded-br-md bg-accent-500"
                     : "bg-white text-black rounded-2xl rounded-bl-md shadow-sm border border-gray-100"
